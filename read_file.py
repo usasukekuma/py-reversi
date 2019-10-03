ef906845ff95d2117305079b468b2815d17809de
usasuke <kurosuke.yoshino@gmail.com>
2019-10-03 16:01:57 +0900

    listカラ空白を削除

--- read_file.py
+++ read_file.py
@@ -2,24 +2,28 @@ from game_master import *
 import pandas as pd
 import itertools
 
-
-othello = game_master()
-df = pd.read_csv('a.csv', header=None)  # 文字列が含まれるので
-tmp = df.values.tolist()
-print(tmp)
-print(list(itertools.chain.from_iterable(tmp)))
-
-
 list_bwin = []
 list_wwin = []
 list_dwin = []
-tmp_b = []
+n_f = [list_bwin, list_wwin, list_dwin]
+tmp_count = []
+
+othello = game_master()
+df = pd.read_csv('a.csv', header=None) # 文字列が含まれるので
+df = df.replace('\r\n', '', regex=True)
+tmp = df.values.tolist()
+tmp = list(itertools.chain.from_iterable(tmp))
+tmp_1 = [x for x in tmp if x]
+print(tmp_1)
+
+
+
+
+tmp_cb = [i for i, x in enumerate(tmp) if x == 'WB']
+tmp_cw = [i for i, x in enumerate(tmp) if x == 'WW']
+tmp_cd = [i for i, x in enumerate(tmp) if x == 'WD']
+
 tmp_w = []
 tmp_d = []
-n_f = [list_bwin, list_wwin, list_dwin]
-n_s = [tmp_b, tmp_w, tmp_d]
-
-
-
-
+tmp_b = []
 
