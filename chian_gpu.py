58556c751561dbba7b95f677a0231cc6771bb1a2
usasuke.d <kurosuke.yoshino@gmail.com>
2019-10-25 19:32:23 +0900

    nyo

--- chian_gpu.py
+++ chian_gpu.py
@@ -1,59 +1,74 @@
-import chainer
-import chainer.links as L
-import chainer.function as F
-import numpy as np
-from chainer import Chain, Link, ChainList,datasets, iterators,cuda
-from chainer import Function, report, training, utils, Variable, optimizers, serializers
-from chainer.training import extensions
 from read_file import *
+from chainer.datasets import TupleDataset, split_dataset_random
+from chainer.iterators import SerialIterator
+import chainer
+from chainer import iterators,optimizers,training,cuda
+import chainer.links as L
+import chainer.functions as F
 
-n_input = 64
+n_in = 64
 n_hidden = 100
-n_output = 65
+n_out = 65
+gpu_id = 0
 
-class Reversi(Chain):
+class N(chainer.Chain):
+
     def __init__(self):
         super().__init__()
         with self.init_scope():
-            self.l1 = L.Linear(n_input, n_hidden)
+            self.l1 = L.Linear(n_in, n_hidden)
             self.l2 = L.Linear(n_hidden, n_hidden)
-            self.l3 = L.Linear(n_hidden, n_output)
+            self.l3 = L.Linear(n_hidden, n_out)
 
     def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        y = F.relu(self.l3(h2))
-        return y
-class Classfilter(Chain):
-    def __init__(self, predictor):
-        super(Classfilter, self).__init__(predictor=predictor)
-
-    def __call__(self, x, t):
-        y = self.predictor(x)
-        loss = F.softmax_cross_entropy(y, t)
-        accuracy = F.accuracy(y, t)
-        report({'loss':loss, 'accuracy':accuracy},self)
-        return loss
+        h = F.relu(self.l1(x))
+        h = F.relu(self.l2(h))
+        h = self.l3(h)
+        return h
 
 
-cuda.Device(0).use()
-xp = cuda.cupy
-X = xp.array(input_board, dtype=np.float32)
-y = xp.array(input_stone, dtype=np.int32)
+# ------データ作成---------
+x = np.array(input_board, 'float32')
+t = np.array(output_stone, 'int32')
+# read_fileと一連託生
+dataset = TupleDataset(x, t)
+ss = len(dataset)
+split_at = int(ss*0.8)
+train, test = split_dataset_random(dataset, split_at, seed=0)
+#  すべての試合の８割を訓練と検証に、、
+train_iter = SerialIterator(train, batch_size=100, repeat=True, shuffle=True)
+#  訓練データを100個＝１セットに　シャッフルもするお！
+valid_iter = iterators.SerialIterator(test, batch_size=50, shuffle=False, repeat=False)
+#  ------end------
 
-train = datasets.TupleDataset(X, y)  # 学習用データを作る datasetsは学習用データを作ってくれる tupleで形を決めている
-train_iter = iterators.SerialIterator(train, batch_size=100)  # iteratoorsは学習データをシャッフルしたりすてくれる
-# バッチサイズはデータを分けるサイズを指定する　１００ずつ
-model = Classfilter(Reversi())
 
-model.to_gpu()
-optimizer = optimizers.SGD()  # SGD法を選択
+N = N()  # ネットをつくるお
+model = L.Classifier(N)  # classfierのデフォ損失関数はF.softmax_cross_entropy
+optimizer = optimizers.SGD(lr=0.1)  # 勾配関数
 optimizer.setup(model)
-
-updater = training.StandardUpdater(train_iter, optimizer)
-trainer = training.Trainer(updater, (1000, 'epoch'), out='result')
+updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
+#  updater イテレータ・オプティマイザを統括し、順伝播・損失・逆伝播の計算、そしてパラメータの更新（オプティマイザの呼び出し）という、
+#  訓練ループ内の定型的な処理を実行します。 by tutorial
+trainer = training.Trainer(updater, (1000, 'epoch'), out='results/'+result_out)
+#  trainer アップデータを受け取り、訓練全体の管理を行います。イテレータを用いてミニバッチを繰り返し作成し、オプティマイザを使ってネットワークのパラメータを更新します。
+#  訓練の終了タイミングの決定や、設定されたエクステンションの呼び出しも担います
 
 trainer.extend(extensions.ProgressBar())
-trainer.run()
+trainer.extend(extensions.Evaluator(valid_iter, model, device=gpu_id))
+trainer.extend(extensions.LogReport(trigger=(50, 'epoch'),log_name='log'))
 
+'''
+trainer.extend(extensions.Evaluator(test, model, -1))
+trainer.extend(extensions.PrintReport(
+    ['epoch', 'main/loss', 'main/accuracy', 'validation/main/loss', 'validation/main/accuracy', 'elapsed_time']))
+
+trainer.extend(extensions.PlotReport(['fc1/W/grad/mean'], x_key='epoch', file_name='mean.png'))
+trainer.extend(extensions.PlotReport(['main/loss', 'val/main/loss'], x_key='epoch', file_name='loss.png'))
+trainer.extend(
+    extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
+'''
+
+trainer.run()
+print('学習は終わった。保存する')
 serializers.save_npz(saving_name, model)
+print('モデルを'+saving_name+'で保存しました')
