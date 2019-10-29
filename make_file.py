0cd09f0effef21b816a064080b617d0e3ed46be6
usasuke.d <kurosuke.yoshino@gmail.com>
2019-10-30 08:59:27 +0900

    make?read

--- make_file.py
+++ make_file.py
@@ -40,13 +40,7 @@ def xy_converter(x, y):
         z = x + y*8
     return z
 
-def csv_list(x, y, color):
-    """"
-    list_c.append(othello.board_copy())
-    """
-    list_c.append(color)
-    z = xy_converter(x, y)
-    list_c.append(z)
+
 
 def new_csv(f_name):
     with open(f_name, 'w', newline='') as f:
@@ -92,12 +86,10 @@ if __name__ == "__main__":
     print('試合数を選ぶなっし(0以外を入力してくださいなっし)')
     battle_time = int(input())
     print(str(battle_time)+'回ではじめるなっし')
-    append_l = csv_list
     list_c = []
     for n in range(0, battle_time):
         list_a = []
         othello = game_master()
-        othello.view()
         i = 0
         k = 0
         kihu_count = 0
@@ -110,12 +102,16 @@ if __name__ == "__main__":
                 can_put_list = othello.can_put_list(BLACK)
                 if not can_put_list == []:
                     x, y = player_1(can_put_list, current_board, kihu_count)
-                    print(can_put_list)
-                    append_l(x, y, color)
+                    list_c.append(color)
+                    z = xy_converter(x, y)
+                    list_c.append(z)
                 else:
                     i += 1
                     x, y = 8, 8
-                    append_l(x, y, color)
+                    list_c.append(color)
+                    z = xy_converter(x, y)
+                    list_c.append(z)
+
                     continue
             #  白のターン
             elif turn == WHITE:
@@ -124,11 +120,16 @@ if __name__ == "__main__":
                 can_put_list = othello.can_put_list(WHITE)
                 if not can_put_list == []:
                     x, y= player_2(can_put_list, current_board, kihu_count)
-                    append_l(x, y, color)
+                    list_c.append(color)
+                    z = xy_converter(x, y)
+                    list_c.append(z)
+
                 else:
                     i += 1
                     x, y = 8, 8
-                    append_l(x, y, color)
+                    list_c.append(color)
+                    z = xy_converter(x, y)
+                    list_c.append(z)
                     continue
             othello.put_stone(x, y, turn)
 
@@ -139,17 +140,20 @@ if __name__ == "__main__":
                 k += 50
             if othello.can_put_list(WHITE) == []:
                 k += 50
-        WIN_info = None
         tmp_w, score_B, score_W = othello.end()
         print(score_B,score_W)
-        if score_B is score_W:
-            WIN_info = DRAW
-        elif score_B > score_W:
-            WIN_info = b_WIN
+        wscore = score_B - score_W
+        if wscore == 0:
+            list_c.append(DRAW)
+            print('DRAW')
+        elif wscore >= 1:
+            list_c.append(b_WIN)
+            print('WIN')
         else:
-            WIN_info = b_LOSE
-        list_c.append(WIN_info)
+            list_c.append(b_LOSE)
+            print('Lose')
         list_c.append('\n')
+        print(list_c[-2])
     print('saving..........')
     f_save(f_name)
     print('complete!')
--- read_file.py
+++ read_file.py
@@ -30,6 +30,14 @@ def conve(put_st):
     t_x, t_y = for_convert[put_st]
     return t_x, t_y
 
+ucon = {0: (0, 0), 1: (1, 0), 2: (2, 0), 3: (3, 0), 4: (4, 0), 5: (5, 0), 6: (6, 0),7: (7, 0),
+        8: (0, 1), 9: (1, 1), 10: (2, 1), 11: (3, 1), 12: (4, 1), 13: (5, 1), 14: (6, 1),'H2': (7, 1),
+            'A3': (0, 2), 'B3': (1, 2), 'C3': (2, 2), 'D3': (3, 2), 'E3': (4, 2), 'F3': (5, 2), 'G3': (6, 2),'H3': (7, 2),
+            'A4': (0, 3), 'B4': (1, 3), 'C4': (2, 3), 'D4': (3, 3), 'E4': (4, 3), 'F4': (5, 3), 'G4': (6, 3),'H4': (7, 3),
+            'A5': (0, 4), 'B5': (1, 4), 'C5': (2, 4), 'D5': (3, 4), 'E5': (4, 4), 'F5': (5, 4), 'G5': (6, 4),'H5': (7, 4),
+            'A6': (0, 5), 'B6': (1, 5), 'C6': (2, 5), 'D6': (3, 5), 'E6': (4, 5), 'F6': (5, 5), 'G6': (6, 5),'H6': (7, 5),
+            'A7': (0, 6), 'B7': (1, 6), 'C7': (2, 6), 'D7': (3, 6), 'E7': (4, 6), 'F7': (5, 6), 'G7': (6, 6),'H7': (7, 6),
+            'A8': (0, 7), 'B8': (1, 7), 'C8': (2, 7), 'D8': (3, 7), 'E8': (4, 7), 'F8': (5, 7), 'G8': (6, 7),'H8': (7, 7)}
 
 #  ファイル名とか初期設定
 tmp_count = []
@@ -54,25 +62,28 @@ df = df.replace('\n', '', regex=True)
 tmp = df.values.tolist()
 print('converting to listなっし')
 tmp1 = [x for a in tmp for x in a]
+print(tmp1)
 tmp_2 = [x for x in tmp1 if x]
+print(tmp_2)
 print('盤面を復元するなっし！')
 wwww = 0
-for c in tmp_2:
-    if c == b_WIN:
+
+for che in tmp_2:
+    if che == b_WIN:
         list_bwin_battle.extend(tmp_3)
         list_bwin_battle.append(END)
         tmp_3 = []
-        wwww +=1
-    elif c == b_LOSE:
+        wwww += 1
+    elif che == b_LOSE:
         list_wwin_battle.extend(tmp_3)
         list_wwin_battle.append(END)
         tmp_3 = []
-    elif c == DRAW:
+    elif che == DRAW:
         list_dwin_battle.extend(tmp_3)
         list_dwin_battle.append(END)
         tmp_3 = []
     else:
-        tmp_3.append(c)
+        tmp_3.append(che)
 
 #  ボード復元
 if sha == 'b_win' or 'b':
@@ -92,6 +103,7 @@ for d in shi:
     elif d == END:
         judge, score_B, score_W = othello.end()
         if ttt == 'n':
+            print(score_B, score_W)
             list_bboard.extend(copy.deepcopy(t_list_bboard))
             list_bstone.extend(copy.deepcopy(t_list_bstone))
             t_list_bboard = []
@@ -103,7 +115,7 @@ for d in shi:
             continue
         elif ttt == 'y':
             print('b')
-            print(score_B,score_W)
+            print(score_B, score_W)
             if score_B - score_W >= score_point:
                 print('a')
                 print(score_B,score_W)
@@ -143,6 +155,7 @@ for d in shi:
             t_list_wstone.append(d)
             ax, ay = conve(int(d))
         othello.put_stone(ax, ay, turn)
+        continue
 print('復元は終わったなっし')
 print(wb)
 if sha == 'black_win' or 'b' or 'black':
@@ -152,5 +165,6 @@ elif sha == 'white_win' or 'w' or 'white':
     input_board = list_wboard
     output_stone = list_wstone
 print(wwww)
+print(tmp_2.count(600))
 print(input_board)
 print(output_stone)
