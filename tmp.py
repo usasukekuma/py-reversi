52c794058d65491defc4ed14b9426d18b1eacbb3
usasuke.n <kurosuke.yoshino@gmail.com>
2019-11-11 19:42:21 +0900

    gui

--- tmp.py
+++ tmp.py
@@ -1,3 +1,17 @@
-import subprocess
-cmd = 'shutdown -s -f -t 0'
-subprocess.run(cmd, shell=True)
+import wx
+
+class reversiGUI(wx.Frame):
+    def __init__(self):
+        super(reversiGUI,self).__init__()
+        self.first_ui()
+
+
+    def first_ui(self):
+        frame = wx.Frame(None, -1, 'Hello,World!',size=(1500,800))
+        frame.SetTitle('オセロ')
+        frame.Show()
+
+
+app = wx.App
+reversiGUI()
+app.MainLoop()
